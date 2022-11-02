ed02945254a13f78b8e3c593544a5f3ad3c72339
Daniel Jonas <83048344+02danieljonas@users.noreply.github.com>
2022-11-02 10:11:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const HTMLText = document.getElementById("HTMLText");
+
+const HTMLTextValues = [
+    "a computer science student",
+    "that guy",
+    "Daniel Jonas",
+];
+
+const speed = 100;
+
+let valuePos = 0;
+let txt = HTMLTextValues[0];
+
+function changeValue() {
+    if (HTMLText.innerHTML.length < txt.length) {
+        HTMLText.innerHTML += txt.charAt(HTMLText.innerHTML.length);
+        HTMLText.innerHTML.length++;
+        setTimeout(changeValue, speed);
+    } else {
+        setTimeout(deleteValue, speed * 6);
+    }
+}
+
+function deleteValue() {
+    if (HTMLText.innerHTML.length != 0) {
+        HTMLText.innerHTML = HTMLText.innerHTML.slice(0, -1);
+        setTimeout(deleteValue, speed);
+    } else {
+        HTMLText.innerHTML.length = 0;
+        changeText();
+        setTimeout(changeValue, speed * 6);
+    }
+}
+
+function changeText() {
+    valuePos < HTMLTextValues.length - 1 ? valuePos++ : (valuePos = 0);
+    // if (valuePos < HTMLTextValues.length - 1) {
+    //     valuePos++;
+    // } else {
+    //     valuePos = 0;
+    // }
+    txt = HTMLTextValues[valuePos];
+}
+
+changeValue();
